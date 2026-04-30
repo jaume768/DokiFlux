8e45930f4309bd72ab4adba68aa9e7c4aa623910
jaume768 <jaumefernandezsunyer10@gmail.com>
2026-04-30 12:42:28 +0200

    quitar testimonios

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -27,7 +27,6 @@ export default function LandingPage() {
         <DualPersona />
         <ComparisonV2 />
         <TemplatesShowcase />
-        <Testimonials />
         <FinalCTA />
       </main>
       <Footer />
